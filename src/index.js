e99f8f4208e62c56a2fa9d8c870d2892914d049e
Teerapong Saisrikaew <teerapong.saisrikaew250819@gmail.com>
2023-01-10 00:21:54 +0700

    refactor code with useContext

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from './context/books';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
